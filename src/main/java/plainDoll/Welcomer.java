7f6e4bcc84cb531507653bcc8128ccee295aa784
Vadim Yushmanov <v_yushmanov@mail.ru>
2023-03-20 02:49:43 +0400

    updated nesus

--- src/main/java/plainDoll/Welcomer.java
+++ src/main/java/plainDoll/Welcomer.java
@@ -1,16 +1,22 @@
 package plainDoll;
 
-public class Welcomer{
+public class Welcomer {
 	public String sayWelcome() {
+
 		return "Welcome home, good hunter. What is it your desire?";
 	}
 	public String sayFarewell() {
+
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
-	public String sayNeedGold(){
+	public String sayNeedGold() {
+
 		return "Not enough gold";
 	}
-	public String saySome(){
-		return "something in the way";
+	public String saySome() {
+
+		return "something in the way";	}
+	public String lateGame() {
+		return "Good hunter... Your presence somehow soothes... I sense the ancient echoes, they course your veins...";
 	}
 }
--- src/test/java/plainDoll/WelcomerTest.java
+++ src/test/java/plainDoll/WelcomerTest.java
@@ -30,4 +30,9 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+
+	@Test
+	public void welcomerLateGame() {
+		assertThat(welcomer.lateGame(), containsString("hunter"));
+	}
 }
